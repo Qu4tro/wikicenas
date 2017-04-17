8da7e05321f6f6879dce6681b910e231109d75d8
Pedro Lima <pedrolima.0000@gmail.com>
2017-04-17 15:47:48 +0100

    faltavam coisas

--- src/interface.h
+++ src/interface.h
@@ -1,23 +1,19 @@
 #include <stdlib.h>
 
-long all_articles(TAD_istruct qs);
+typedef struct TCD_istruct * TAD_istruc;
 
-long unique_articles(TAD_istruct qs);
+TAD_istruc init();
+TAD_istruc load(TAD_istruc qa , int nsnaps , char* snaps_paths[]);
 
-long all_revisions(TAD_istruct qs);
-
-long* top_10_contributors(TAD_istruct qs);
-
-char* contributor_name(long contributor_id, TAD_istruct qs);
-
-long* top_20_largest_articles(TAD_istruct qs);
-
-long* article_title(long article_id, TAD_istruct qs);
-
-long* top_N_articles_with_more_words(int n, TAD_istruct qs);
-
-char** title_with_prefix(char* prefix, TAD_istruct qs);
-
-char* article_timestamp(long article_id, long revision_id, TAD_istruct qs);
+ long all_articles(TAD_istruct qs);
+ long unique_articles(TAD_istruct qs);
+ long all_revisions(TAD_istruct qs);
+ long* top_10_contributors(TAD_istruct qs);
+ char* contributor_name(long contributor_id, TAD_istruct qs);
+ long* top_20_largest_articles(TAD_istruct qs);
+ char* article_title(long article_id, TAD_istruct qs);
+ long* top_N_articles_with_more_words(int n, TAD_istruct qs);
+ char** titles_with_prefix(char* prefix, TAD_istruct qs);
+ char* article_timestamp(long article_id, long revision_id, TAD_istruct qs);
 
 TAD_istruct clean(TAD_istruct qs);
